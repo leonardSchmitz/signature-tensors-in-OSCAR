1504dd0e7f77690551545b840d320e288bf78d25
leonardSchmitz <leonardschmitz@eduroam-141-23-161-50.wlan.tu-berlin.de>
2026-02-11 13:50:59 +0100

    tests for pwln and type converting

--- test/TruncatedTensorAlgebraTest.jl
+++ test/TruncatedTensorAlgebraTest.jl
@@ -1,10 +1,10 @@
 
 @testset "Truncated Tensor Algebra Tests :iis" begin
-    d = 6        # path dimension
-    k = 5        # truncation level
-    T = TruncatedTensorAlgebra(QQ,d,k)
 
     @testset "Constructor TTA" begin
+       d = 6        # path dimension
+       k = 5        # truncation level
+       T = TruncatedTensorAlgebra(QQ,d,k)
        @test T == TruncatedTensorAlgebra(QQ,d,k,sequence_type=:iis)
        @test sequence_type(T) == :iis
        @test base_dimension(T) == d
@@ -33,6 +33,7 @@
     end
 
     @testset "Axis constructor in TTA for QQ" begin
+        T = TruncatedTensorAlgebra(QQ,6,5);
         Caxis_d = sig(T,:axis);
         @test parent(Caxis_d) == T
         @test zero(T) + zero(T) == zero(T)
@@ -53,50 +54,65 @@
         @test inv(inv(Caxis_d)) == Caxis_d
     end
 
+    @testset "Axis constructor in TTA for polynomial rings" begin
+        d = 6;
+        R, a = polynomial_ring(QQ, :a => (1:d, 1:d));
+        T = TruncatedTensorAlgebra(R,d,5);
+        Caxis_d = sig(T,:axis);
+        @test parent(Caxis_d) == T
+        @test zero(T) + zero(T) == zero(T)
+        @test Caxis_d == sig(T,:axis,algorithm=:Chen)
+        @test Caxis_d == sig(T,:axis,algorithm=:AFS19) 
+        for i in (2:d-1)
+            @test Caxis_d[i] == one(R)
+            @test Caxis_d[i,i-1] == zero(R)
+            @test Caxis_d[i,i+1] == one(R)
+        end 
+        @test zero(T) + Caxis_d == Caxis_d
+        @test one(T)*Caxis_d == Caxis_d
+        @test Caxis_d*one(T) == Caxis_d
+        @test inv(Caxis_d)*Caxis_d == one(T)
+        @test Caxis_d*inv(Caxis_d) == one(T)
+        @test inv(inv(Caxis_d)) == Caxis_d
+    end
+
     @testset "Pwln constructor in TTA" begin
-        ms = rand((1:7),5);
+        d = 6; k = 5; number_tests = 5;
+        T = TruncatedTensorAlgebra(QQ,d,k);
+        ms = rand((1:7),number_tests); # maximal 7 number of segments
         As = [QQ.(rand((-20:20),d,m)) for m in ms];
         for A in As
+          m = size(A)[2]  # number of segments in A
           S = sig(T,:pwln,coef=A); 
           @test S == sig(T,:pwln,coef=A,algorithm=:Chen); 
           @test S == sig(T,:pwln,coef=A,algorithm=:congruence); 
+          C = sig(TruncatedTensorAlgebra(QQ,m,k),:axis); # axis tensor in dim m
+          @test S == A*C
           @test zero(T) + S == S
           @test one(T)*S == S
           @test S*one(T) == S
           @test inv(S)*S == one(T)
           @test S*inv(S) == one(T)
           @test inv(inv(S)) == S
-          m = size(A)[2]
           for m1 in 1:m-1     # decompose via Chen
             A1 = A[:,1:m1]
-            A2 = A[:,m1+1:end]
+            A2 = A[:,m1+1:m]
             @test hcat(A1,A2) == A
             @test sig(T,:pwln,coef=A) == sig(T,:pwln,coef=A1)*sig(T,:pwln,coef=A2)
           end
         end 
-    end        
-              
+    end    
     
-
-    #m = 7        # number of segments
-    #A = QQ.(rand((-20:20),d,m)) 
-    #C = sig(TruncatedTensorAlgebra(QQ,m,k),:axis); 
-
-    #@test A*Caxis_d == S
-    #    Cmono_d = sig(T,:mono)
-    #    for Si in [Caxis_d,S,one(T)]
-    #        @test zero(T) + Si == zero(T)
-    #        @test one(T)*Si == Si
-    #        @test Si*one(T) == Si
-    #        @test inv(Si)*Si == one(T)
-    #        @test Si*inv(Si) == one(T)
-    #        @test inv(inv(Si)) == Si
-    #    end
-    #    for m1 in 1:m-1     # decompose via Chen
-    #        A1 = A[:,1:m1]
-    #        A2 = A[:,m1+1:end]
-    #        @test hcat(A1,A2) == A
-    #        @test sig(T,:pwln,coef=A) == sig(T,:pwln,coef=A1)*sig(T,:pwln,coef=A2)
-    #    end
-    
+    @testset "Converting base algebra via matrix tensor congruence" begin
+        d = 6; m = 5; k = 4;
+        R, a = polynomial_ring(QQ, :a => (1:d, 1:m));
+        TmQQ = TruncatedTensorAlgebra(QQ,m,k);
+        TdR = TruncatedTensorAlgebra(R,d,k);
+        TmR = TruncatedTensorAlgebra(R,m,k);
+        C = sig(TmQQ,:axis);
+        S = sig(TdR,:pwln,coef=a);
+        @test S == a*C
+        A = QQ.(rand((-20:20),d,m));
+        @test A*sig(TmR,:axis) ==  sig(TdR,:pwln,coef=A);
+    end 
 end

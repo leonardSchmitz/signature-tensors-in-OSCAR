3c7a32dfd19389f6943c5cbe6941f05d1939cea3
leonardSchmitz <leonardschmitz@Leonards-MacBook-Pro.local>
2026-02-16 15:27:08 +0100

    tests for mode-product

---
+++ test/tensorOperationsTest.jl
@@ -0,0 +1,21 @@
+
+@testset "Matrix-Tensor mode-product" begin
+  number_tests = 10;
+  ms = rand((1:15),number_tests); # maximal 7 number of segments
+  ds = rand((1:15),number_tests); # maximal 7 number of segments
+  Gs = [QQ.(rand((-20:20),ms[i],ms[i],ms[i])) for i in (1:number_tests)];
+  As = [QQ.(rand((-20:20),ds[i],ms[i])) for i in (1:number_tests)];
+  for i in (1:number_tests)
+    for mu in (1:3)
+      @test matrix_tensor_multiply_3(As[i], Gs[i], mu) == matrix_tensor_multiply(As[i], Gs[i], mu);
+    end
+  end
+
+  As = [generic_transform_GL(ms[i]) for i in (1:number_tests)];
+  for i in (1:number_tests)
+    for mu in (1:3)
+      AG = matrix_tensor_multiply(As[i], Gs[i], mu);
+      @test matrix_tensor_multiply(inv(As[i]), AG, mu) == Gs[i];
+    end
+  end
+end

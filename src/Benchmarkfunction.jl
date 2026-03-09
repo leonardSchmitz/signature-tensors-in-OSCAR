8dff7572a1f7bbf77deb1c959e55614fb8489aa0
GabrielRiffoJ <gabrielriffo22@gmail.com>
2026-03-09 14:18:07 +0100

    Benchmark_kill

--- src/Benchmarkfunction.jl
+++ src/Benchmarkfunction.jl
@@ -1,6 +1,19 @@
 export 
     benchmark_signature
 
+function run_with_timeout_julia(f::Function, timeout::Int=5)
+    t = Threads.@spawn f()  # Function in a diferent Thread
+    for i in 1:timeout
+        if istaskdone(t)  # check if the function has ended
+            return fetch(t)  # return the result
+        end
+        sleep(1)  # wait 1 seconds and then checks
+    end
+    #if we reach here the function has no ended
+    return false
+end
+
+
 function benchmark_signature(ds::Vector{Int}, ms::Vector{Int}, k::Int; 
                              signature_path::Symbol=:pwln,
                              seq_type::Symbol=:iis,
@@ -15,15 +28,24 @@ function benchmark_signature(ds::Vector{Int}, ms::Vector{Int}, k::Int;
         for m in ms
             T = TruncatedTensorAlgebra(QQ, d, k, seq_type)
             A = QQ.(rand(-20:20, d, m))
+            result = run_with_timeout_julia(() -> sig(T, signature_path, coef=A, algorithm=algorithm), Seconds)
 
-            # Crear benchmarkable y correrlo
-            benchset = @benchmarkable sig($T, $signature_path, coef=$A, algorithm=$algorithm) seconds=Seconds samples=num_samples
-            bench = run(benchset)
+            if result === false
+                    wall_time[(d,m)] = -1* Seconds    
+                    gc_time[(d,m)]   = -1
+                    memory[(d,m)]    =  -1
+            else
+                            # Crear benchmarkable y correrlo
+                benchset = @benchmarkable sig($T, $signature_path, coef=$A, algorithm=$algorithm) seconds=Seconds samples=num_samples
+                bench = run(benchset)
 
-            # Guardar medianos en nanosegundos / bytes
-            wall_time[(d,m)] = median(bench).time / 1e9   
-            gc_time[(d,m)]   = median(bench).gctime / 1e9 
-            memory[(d,m)]    = median(bench).memory  / 1e6       
+                # Guardar medianos en nanosegundos / bytes
+                wall_time[(d,m)] = median(bench).time / 1e9   
+                gc_time[(d,m)]   = median(bench).gctime / 1e9 
+                memory[(d,m)]    = median(bench).memory  / 1e6       
+   #              println("The function is ready for Benchmark")
+            end
+
         end
     end
 
